cc70f00bc5b0f36f19ec19105648176f37966d7b
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-06-12 22:34:16 -0500

    fixed asynch problems with the console.table function and the inquirer prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 var mysql = require("mysql");
-var inquirer = require("inquirer");
 var cTable = require("console.table");
+var inquirer = require("inquirer");
+
 
 //Set up connection to mySQL database, passwords can be loaded in .env files that will pull in.
 var connection = mysql.createConnection({
@@ -15,11 +16,12 @@ var connection = mysql.createConnection({
   // Your database
   database: "bamazon"
 });
+
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   readProducts();
-});
+})
 
 //function that queries the database
 function readProducts() {
@@ -27,15 +29,17 @@ function readProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
-    displayTable(res);
+    displayTable(res,promptUser);
   });
 
   //Function that displays SQL table in command line using console.table package.
-  function displayTable(res) {
+  function displayTable(res, promptUser) {
     var table = cTable.getTable(res);
     console.log(table);
+    promptUser();
   }
   //Prompt the user with messages regarding their product id and how many units they would like to purchase.
+  function promptUser(){
   inquirer
     .prompt([
       // Here we give the user a list of product IDs to choose from.
@@ -43,19 +47,21 @@ function readProducts() {
         type: "input",
         message: "Which product are you interested in purchasing?",
         name: "productID"
-      },
-      {
-        type: "input",
-        message: "How many units of the product would you like to purchase?",
-        name: "quantity"
       }
+      // },
+      // {
+      //   type: "input",
+      //   message: "How many units of the product would you like to purchase?",
+      //   name: "quantity"
+      // }
     ])
-    .then(function(inquirerResponse) {
-      // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-      if (inquirerResponse.confirm) {
-        console.log("You chose " + inquirerResponse.choices + ".");
+    .then(function(response) {
+      // If the response confirms, we displays the response's username and pokemon from the answers.
+      if (response) {
+        console.log("You chose " + response.name + ".");
       } else {
         console.log("Stuff no work");
       }
     });
+  }
 }
