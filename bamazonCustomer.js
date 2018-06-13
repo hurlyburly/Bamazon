02fa67bacb9038974de79cd0ed29e68f1c059b47
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-06-13 14:57:22 -0500

    adding in further logic for prompts and SQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var mysql = require("mysql");
 var cTable = require("console.table");
 var inquirer = require("inquirer");
+var bodyParser = require('body-parser')
+
 
 
 //Set up connection to mySQL database, passwords can be loaded in .env files that will pull in.
@@ -36,10 +38,19 @@ function readProducts() {
   function displayTable(res, promptUser) {
     var table = cTable.getTable(res);
     console.log(table);
-    promptUser();
+    promptUser(table);
   }
-  //Prompt the user with messages regarding their product id and how many units they would like to purchase.
-  function promptUser(){
+  function checkProducts(response) {
+  console.log("Checking inventory in Bamazon. \n");
+var query=connection.query("SELECT * FROM products WHERE ??",{item_id:response.productID},function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log("There are currently "+ res.stock_quantity+ " units of " + res.product_name+".");
+  });
+  console.log(query.sql);
+}
+//Prompt the user with messages regarding their product id and how many units they would like to purchase.
+  function promptUser(table){
   inquirer
     .prompt([
       // Here we give the user a list of product IDs to choose from.
@@ -47,21 +58,23 @@ function readProducts() {
         type: "input",
         message: "Which product are you interested in purchasing?",
         name: "productID"
+      },
+      {
+        type: "input",
+        message: "How many units of the product would you like to purchase?",
+        name: "quantity"
       }
-      // },
-      // {
-      //   type: "input",
-      //   message: "How many units of the product would you like to purchase?",
-      //   name: "quantity"
-      // }
     ])
-    .then(function(response) {
+    .then(function(response,checkProducts) {
       // If the response confirms, we displays the response's username and pokemon from the answers.
-      if (response) {
-        console.log("You chose " + response.name + ".");
+      if (response.productID) {
+        console.log("You've chosen to purchase "+response.quantity+" unit(s) of product #" + response.productID + ".");
+        checkProducts(response);
       } else {
         console.log("Stuff no work");
       }
     });
   }
 }
+
+
